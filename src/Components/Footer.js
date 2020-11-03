23d08f29df91bed46e3df10bcbbb6953612e837d
Irfan Irawan Sukirman <dadang.kotz@gmail.com>
2020-11-03 20:25:18 +0700

    init years copyright

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 <a title="Tim Baker" href="http://www.timbakerdev.com/">Tim Baker</a></li>
+              <li>&copy; Copyright {(new Date().getFullYear())} <a title="Irfan Irawan Sukirman, S.Kom." href="https://about-me-irfan.netlify.app/">Irfan Irawan Sukirman, S.Kom</a></li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
