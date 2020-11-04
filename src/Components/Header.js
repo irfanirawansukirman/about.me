03b8b5de5d3fcd438de9e4d6600fa474688369f1
Irfan Irawan Sukirman <dadang.kotz@gmail.com>
2020-11-04 14:29:46 +0700

    final

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -30,7 +30,7 @@ class Header extends Component {
             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
             <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
             <li><a href="https://medium.com/@irfanirawansukirman" target="_blank">Blog</a></li>
-            <li><a href="https://www.youtube.com/" target="_blank">Youtube</a></li>
+            {/* <li><a href="https://www.youtube.com/" target="_blank">Youtube</a></li> */}
          </ul>
       </nav>
 
